01136bfa3fe4641640478b8fd838c46c9570525a
Krutik <58563133+reboot13-git@users.noreply.github.com>
2021-03-08 15:07:38 +0530

    Update addition.c

--- addition.c
+++ addition.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-
+/// Addition of 2 numbers
 int main() {
-int a,b,c;
-printf("Enter 2 numbers\n");
-scanf("%d %d", &a, &b);
+  int a,b,c;
+    printf("Enter 1st Number\n");
+    scanf("%d", &a);
+
+    printf("Enter 2nd Number\n");
+    scanf("%d", &b);
 c = a + b;
     printf("Addition of 2 numbers: %d", c);
     return 0;
