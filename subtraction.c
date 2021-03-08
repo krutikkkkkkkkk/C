2ffc0efff44206e4eb8050ce8215bf9bac534462
Krutik <58563133+reboot13-git@users.noreply.github.com>
2021-03-08 15:09:19 +0530

    Update subtraction.c

--- subtraction.c
+++ subtraction.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 int main() {
-int a,b,c;
-printf("Enter 2 numbers\n");
-scanf("%d %d", &a, &b);
+  int a,b,c;
+    printf("Enter any Number\n");
+    scanf("%d", &a);
+
+    printf("Enter another Number\n");
+    scanf("%d", &b);
 c = a - b;
     printf("Subtraction of 2 numbers: %d", c);
     return 0;
