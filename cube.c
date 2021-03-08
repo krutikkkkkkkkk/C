393262f4524758469f871aa75ea1baeebad3faca
Krutik <58563133+reboot13-git@users.noreply.github.com>
2021-03-08 13:40:16 +0530

    Create cube.c

---
+++ cube.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+int a, cube;
+printf("Enter Any Number\n");
+scanf("%d", &a);
+
+printf("Entered number is %d\n", a);
+
+cube = a * a * a;
+    printf("Cube of entered number is: %d", cube);
+    return 0;
+}
