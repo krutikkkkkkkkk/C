9ea89c3143da5de13b79a40af28db8757719da8d
Krutik <58563133+reboot13-git@users.noreply.github.com>
2021-03-08 13:39:39 +0530

    Create square.c

---
+++ square.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+int a, sqr;
+printf("Enter Any Number\n");
+scanf("%d", &a);
+
+printf("Entered number is %d\n", a);
+
+sqr = a * a;
+    printf("Square of entered number is: %d", sqr);
+    return 0;
+}
