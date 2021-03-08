ea2d25a9a4753f968637772033e322ff7f027b70
Krutik <58563133+reboot13-git@users.noreply.github.com>
2021-03-08 15:45:19 +0530

    Update length-of-a-string.c

--- length-of-a-string.c
+++ length-of-a-string.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() { 
+int main(void) { 
     char a[100];
     printf("Enter a string\n");
  scanf("%s", &a);
-
+   printf("Entered string: %s\n", a);
     int length;
     length = strlen(a);
-
+ 
     printf("Length of Entered string is %d", length);
     return 0;
 }
